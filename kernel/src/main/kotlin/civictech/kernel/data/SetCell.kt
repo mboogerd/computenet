65b0a04ab27f82a5a808aa58e53c022faf04f340
Merlijn Boogerd <mlboogerd@gmail.com>
2026-02-26 08:27:57 +0100

    Example Set implementation

---
+++ kernel/src/main/kotlin/civictech/kernel/data/SetCell.kt
@@ -0,0 +1,58 @@
+package civictech.kernel.data
+
+import civictech.kernel.germ.port.FanInlet
+import civictech.kernel.germ.port.FanOutlet
+import civictech.kernel.germ.port.Subscribe
+import civictech.kernel.germ.port.Use
+
+interface SetOps<E> {
+    fun add(element: E)
+    fun remove(element: E)
+}
+
+data class SetDelta<E>(
+    val adds: Set<E>,
+    val dels: Set<E>
+) {
+    fun mergeAddWins(other: SetDelta<E>): SetDelta<E> {
+        val newAdds = adds.union(other.adds)
+        val newDels = dels.union(other.dels).minus(newAdds)
+        return SetDelta(newAdds, newDels)
+    }
+    fun mergeDelWins(other: SetDelta<E>): SetDelta<E> {
+        val newDels = dels.union(other.dels)
+        val newAdds = adds.union(other.adds).minus(newDels)
+        return SetDelta(newAdds, newDels)
+    }
+
+}
+
+interface SetApi<E> {
+    val inlet: Use<SetOps<E>>
+    val outlet: Subscribe<Propagate<SetDelta<E>>>
+}
+
+class SetCell<E> : SetApi<E> {
+    override val inlet = FanInlet.create<SetOps<E>>()
+    override val outlet = FanOutlet.create<Propagate<SetDelta<E>>>()
+
+    private val state = mutableSetOf<E>()
+
+    init {
+        inlet.serve(object : SetOps<E> {
+            override fun add(element: E) {
+                state += element
+                outlet.call.propagate(SetDelta(setOf(element), emptySet()))
+            }
+
+            override fun remove(element: E) {
+                state -= element
+                outlet.call.propagate(SetDelta(emptySet(), setOf(element)))
+            }
+        })
+    }
+
+    companion object {
+        fun <E> create(): SetApi<E> = SetCell()
+    }
+}
